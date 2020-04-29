c8e1b552d2885064da53843ab8e993687295d758
RuslansGit <fcjuvefc@gmail.com>
2020-04-29 12:58:12 -0400

    helloGit.java

--- src/day_1/allGood.java
+++ src/day_1/allGood.java
@@ -3,5 +3,7 @@ package day_1;
 public class allGood {
     public static void main(String[] args) {
         // I hope everything is GOOD
+
+
     }
 }
---
+++ src/gitto/helloGit.java
@@ -0,0 +1,7 @@
+package gitto;
+
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("hello mello git");
+    }
+}
